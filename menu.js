5ab148977c595a5a918d74fa0067e7d065f4f02a
Yevhen Lehenkyi <meister.ramires@gmail.com>
2024-05-06 16:28:54 +0300

    add changes package.json

--- menu.js
+++ menu.js
@@ -4,9 +4,8 @@
   const closeMenuBtn = document.querySelector('.modal-close-btm');
   const closeMenuLink = document.querySelectorAll('.modal-home');
 
-  const toggleMenu = () => {
-    const isMenuOpen =
-      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+  function toggleMenu() {
+    const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
     mobileMenu.classList.toggle('is-open');
 
@@ -14,7 +13,7 @@
       ? 'disableBodyScroll'
       : 'enableBodyScroll';
     bodyScrollLock[scrollLockMethod](document.body);
-  };
+  }
   closeMenuLink.forEach(item => item.addEventListener('click', toggleMenu));
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
