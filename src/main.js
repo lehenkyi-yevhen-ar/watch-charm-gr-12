f1a226aa92617035d4d19f84fa48d258ff526baa
Yevhen Lehenkyi <meister.ramires@gmail.com>
2024-05-07 17:40:31 +0300

    add active header scroll nav

--- src/main.js
+++ src/main.js
@@ -5,3 +5,14 @@ import('./js/menu.js')
       .catch(error => {
         console.error('Error loading menu.js:', error);
       });
+
+      document.addEventListener("DOMContentLoaded", function() {
+        import('.js/header-active.js')
+            .then(module => {
+                console.log("header-active.js imported successfully!");
+            })
+            .catch(error => {
+                console.error("Error importing header-active.js:", error);
+            });
+    });
+    
